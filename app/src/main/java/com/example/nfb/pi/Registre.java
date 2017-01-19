ecd40fa3d148849aab7eda585c01a4518365b3c0
Nestor Frigola <truenoboom@gmail.com>
2017-01-19 12:00:43 +0100

    lalal

--- app/src/main/java/com/example/nfb/pi/Registre.java
+++ app/src/main/java/com/example/nfb/pi/Registre.java
@@ -1,12 +1,16 @@
 package com.example.nfb.pi;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.CheckBox;
 
 public class Registre extends AppCompatActivity {
 
     CheckBox chec;
+    Button registro;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -14,7 +18,19 @@ public class Registre extends AppCompatActivity {
         setContentView(R.layout.registre);
         getSupportActionBar().hide();
 
-        //chec = (CheckBox) findViewById(R.id.ok);
+        chec = (CheckBox) findViewById(R.id.checkBox);
+        registro = (Button) findViewById(R.id.btnRegis);
+
+        registro.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(getApplicationContext(),Inici.class);
+                if (chec.isChecked()){
+                    startActivity(i);
+
+                }
+            }
+        });
 
     }
 }
